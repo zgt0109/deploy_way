1f5d05122d40de0d16dc20a0cc733362909df552
zouguotao <2506526350@qq.com>
2018-04-23 14:10:41 +0800

    update deploy config

--- deploy.sh
+++ deploy.sh
@@ -3,33 +3,37 @@
 # initialize
 GEM_SOURCES_CHINA=https://gems.ruby-china.org/
 GEM_SOURCES_ORIGIN=https://rubygems.org/
-export LC_ALL="en_US.UTF-8"
-export DEBIAN_FRONTEND=noninteractive
+# Public Key zgt.pub
+rsa_pub="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDXqqura67hScy6V+A2r2YEUQXHZiwkFCR/4TLZGHNufYC+0j+2tZC4nTwjCCGrBIVLuE2ZEbvAItUt8XTKVLe+8vzT/oHsnopxCTEwe1bXOj+XeC9PAGZx5tQG9WKps9XqBovV6dw5NfOpjjEEKoTXCIZxlOqL9d94pEDM9hGvc/So1GtOiLEJSwNV19wIdQjTi3fyUhRhRey85usDon1blWilSPpUJQi2FMWhRoQn74DTiHOyR2P/y9sWN8uL/91wxOuxAiiCp1XsrPPNntZk8fTibkiW4pOxiwfI3HBX0XmAPOgDU3aR44qHw0dqQ4Z0CNj9payKUkWiS0sPickh zgt@zgt-Lenovo-Y430P"
+user_name = 'deploy'
+secret_key="15d8929ff6dfdce9b7d900464e07e2e789469fed0400215566a73d1a3161b11e754fdcd878c973faef664d7228c1bd8b3e62a7e4954f966cfbc829de4d85c9eb"
 
-# https://www.brightbox.com/docs/ruby/ubuntu/
-apt-get update
-apt-get install -yq software-properties-common
-apt-add-repository ppa:brightbox/ruby-ng
+# read -p "Public Key? " rsa_pub
+# read -p "Deploy User Name? " user_name
+read -p "Application Name? " app_name
 
-apt-get update && apt-get install -y  --no-install-recommends \
-      build-essential \
-      language-pack-zh-hans \
-      git-core \
-      ruby2.3 ruby2.3-dev\
-      mysql-server mysql-client libmysqlclient-dev \
-      redis-server redis-tools \
-      nodejs nodejs-dev \
-      nginx
+# user
+CHECK_USER=$(grep $user_name /etc/passwd | wc -l)
 
-# Gem
-gem sources --add $GEM_SOURCES_CHINA --remove $GEM_SOURCES_ORIGIN -v
-echo 'gem: --no-document' | tee -a ~/.gemrc
-
+if [ ! $CHECK_USER -ge 1 ]; then
+  useradd -m -G sudo,adm -s /bin/bash $user_name
+  echo "${user_name} ALL=NOPASSWD:ALL" >> /etc/sudoers
+su - $user_name  <<EOF
 # bundler
-gem install bundler
 bundle config mirror.${GEM_SOURCES_ORIGIN%/} ${GEM_SOURCES_CHINA%/}
-
-# nginx
-sed  -i 's/#\s\+\(server_names_hash_bucket_size\)/\1/' /etc/nginx/nginx.conf
+# SSH
+mkdir ~/.ssh
+echo ${rsa_pub} >> ~/.ssh/authorized_keys
+sed  -i "1i # Rails Applicaton Configure\n" ~/.bashrc
+sed  -i "1a export RAILS_ENV=production" ~/.bashrc
+sed  -i "1a export SECRET_KEY_BASE=${secret_key}" ~/.bashrc
+EOF
+fi
+# app
+app_path=/var/www/$app_name
+if [ ! -d $app_path ]; then
+mkdir -p $app_path
+chown -R $user_name:$user_name $app_path
+fi
 
 echo -e "\e[31;43;1m All Done. Have a nice day!   \e[0m "
